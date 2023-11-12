8109f1572c18047f9f4b4480093f56727cfc3d4b
Kundan Kumar Prasad <kundan.k@zolostays.com>
2023-11-12 14:17:48 +0530

    changes as per review

--- Week-6/es6module/calculator.js
+++ Week-6/es6module/calculator.js
@@ -1,7 +1,15 @@
 export const add = (a, b) => {
-  return a + b;
+  if (typeof a == "number" && typeof b == "number") {
+    return a + b;
+  } else {
+    console.warn("Please provide number")
+  }
 }
 
 export const subtract = (a, b) => {
-  return a - b;
+  if (typeof a == "number" && typeof b == "number") {
+    return a - b;
+  } else {
+    console.warn("Please provide number")
+  }
 }
